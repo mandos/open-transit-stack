e645fba8e143ee0aa43e5c8a7bd6cbe33750d5d3
Marek Maksimczyk <marek.maksimczyk@mandos.net.pl>
2026-02-06 12:30:37 +0900

    feat(ippo-web): Add basic map display [WIP]

--- apps/ippo-web/src/app/app.tsx
+++ apps/ippo-web/src/app/app.tsx
@@ -1,51 +1,50 @@
 // Uncomment this line to use CSS modules
 // import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
+// import NxWelcome from './nx-welcome';
+import { MapComponentsProvider, MapLibreMap } from '@mapcomponents/react-maplibre';
+import { LayerSpecification, SourceSpecification, StyleSpecification } from 'maplibre-gl';
 
-import { Route, Routes, Link } from 'react-router-dom';
+// import { Route, Routes, Link } from 'react-router-dom';
+//
+const style: StyleSpecification = {
+  version: 8,
+  layers: [
+    {
+      id: "main",
+      type: "raster",
+      "source": "openStreet",
+    } as LayerSpecification
+  ],
+  sources: {
+    openStreet: {
+      type: "raster",
+      tiles: [
+        "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+      ],
+      minzoom: 4,
+      maxzoom: 15,
+      volatile: false
+
+    } as SourceSpecification,
+  }
+};
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="@mandos-dev/ippo-web" />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
+    <div id="map">
+      <MapComponentsProvider>
+        <MapLibreMap
+          options={{
+            container: 'map', // container id
+            // style: 'https://demotiles.maplibre.org/style.json', // style URL
+            style: style,
+            center: [141.352077, 43.067554], // starting position [lng, lat]
+            zoom: 11, // starting zoom
+          }}
         />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
+      </MapComponentsProvider>
     </div>
+
   );
 }
 
